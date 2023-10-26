1e25cf36d077eaa628183cfc085faa65808a42b8
DESKTOP-N0EE8FR\JChp <bobdylan0331@gmail.com>
2023-10-26 17:53:57 +0900

    redux

--- react-sec18/01-starting-project/src/store/CartProvider.js
+++ react-sec18/01-starting-project/src/store/CartProvider.js
@@ -13,7 +13,7 @@ const cartReducer = (state, action) => {
       state.totalAmount + action.item.price * action.item.amount;
 
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
+      (item) => item.id === action.item.id,
     );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
@@ -36,13 +36,13 @@ const cartReducer = (state, action) => {
   }
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
+      (item) => item.id === action.id,
     );
     const existingItem = state.items[existingCartItemIndex];
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
     if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id);
+      updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
       const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
       updatedItems = [...state.items];
@@ -51,8 +51,12 @@ const cartReducer = (state, action) => {
 
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
+  }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -61,8 +65,12 @@ const cartReducer = (state, action) => {
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
-    defaultCartState
+    defaultCartState,
   );
+
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' });
+  };
 
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: 'ADD', item: item });
@@ -77,6 +85,7 @@ const CartProvider = (props) => {
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
