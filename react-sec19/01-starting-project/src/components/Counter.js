88ef1c933dcd8b9292ad6e1d3ed9d15bd4bc5481
DESKTOP-N0EE8FR\JChp <bobdylan0331@gmail.com>
2023-10-27 16:30:45 +0900

    md

--- react-sec19/01-starting-project/src/components/Counter.js
+++ react-sec19/01-starting-project/src/components/Counter.js
@@ -1,12 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterActions } from '../store/counter';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  const counterButtons = (
+    <div>
+      <button onClick={decrementHandler}>Decrement</button>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={increaseHandler}>Increase by 5</button>
+    </div>
+  );
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      {counterButtons}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- react-sec19/01-starting-project/src/components/Header.js
+++ react-sec19/01-starting-project/src/components/Header.js
@@ -1,22 +1,35 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const logged = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
+  const myMenu = (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">My Products</a>
+        </li>
+        <li>
+          <a href="/">My Sales</a>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {logged && myMenu}
     </header>
   );
 };
